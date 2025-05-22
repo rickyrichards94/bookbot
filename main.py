a49d0bd3b7f5a340a321fa12ab113a5eff172465
rickyrichards94 <57735813+rickyrichards94@users.noreply.github.com>
2025-05-22 17:07:11 -0400

    Added book read to main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text():
+    with open("/home/rickyricky/bookbot/books/frankenstein.txt") as file:
+        book_text = file.read()
+        return book_text
+
+def main():
+    book_text = get_book_text()
+    print(book_text)
+
+main()
