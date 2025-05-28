3aa33b68f3c9766d38af69c7f5c9a0b163e1acbe
rickyrichards94 <57735813+rickyrichards94@users.noreply.github.com>
2025-05-27 20:55:09 -0400

    final changes/assignment for bootdev bookbot

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from stats import word_count, char_count
+import sys
+from stats import (word_count, char_count, book_sort)
 
-def get_book_text():
-    with open("/home/rickyricky/bookbot/books/frankenstein.txt") as file:
-        book_text = file.read()
-        return book_text
+
 
 def main():
-    book_text = get_book_text()
-    print(word_count(book_text), "words found in the document")
-    print(char_count(book_text))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    num_words = word_count(book_text)
+    chars_dict = char_count(book_text)
+    char_sorted_list = book_sort(chars_dict)
+    print_report(book_path, num_words, char_sorted_list)
+
+def get_book_text(path):
+    print(f"Function called with path: {path}")  # Debug line
+    with open(path) as f:
+        return f.read()
+
+def print_report(book_path, num_words, char_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,4 +10,14 @@ def char_count(book_text):
             chars_count[char]+=1
         else:
             chars_count[char] = 1
-    return chars_count    
+    return chars_count
+
+def sort_by_num(d):
+    return d["num"]
+
+def book_sort(chars_final):
+    letter_count = []
+    for ch in chars_final:  
+        letter_count.append({"char": ch, "num": chars_final[ch]})
+    letter_count.sort(reverse=True, key=sort_by_num)
+    return letter_count
